4c18cdb3dde82654a7724cc7c2212b936b7aeb78
Andi Fauji Akbar <faujiakbar@gmail.com>
2018-08-11 00:42:49 +0700

    try connection database manual and add folder vendor

--- app/Http/Middleware/AuthenticateMiddleware.php
+++ app/Http/Middleware/AuthenticateMiddleware.php
@@ -22,7 +22,8 @@ class AuthenticateMiddleware
             $token = $request->header('Authorization');
             if(!$token)
                 throw New Exception('Unauthorized',401);
-            $r = (object) RestCurl::exec('GET',env('AUTH_URI',null),[],$token);
+                // $r = (object) RestCurl::exec('GET',env('AUTH_URI',null),[],$token);
+                $r = (object) RestCurl::exec('GET','https://lentick-api-user-management-dev.azurewebsites.net/auth/check',[],$token);
             if($r->status !== 200){
                 $head = explode("\r\n",$r->header);
                 throw New Exception(substr($head[0],13),$r->status);
