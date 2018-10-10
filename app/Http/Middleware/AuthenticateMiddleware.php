a4ec56f926411da113e7cc08b13e371c3b608e15
Andi Fauzi Akbar <faujiakbar@gmail.com>
2018-10-10 22:57:39 +0700

    add allow header

--- app/Http/Middleware/AuthenticateMiddleware.php
+++ app/Http/Middleware/AuthenticateMiddleware.php
@@ -39,6 +39,7 @@ class AuthenticateMiddleware
 
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
     }
 }
