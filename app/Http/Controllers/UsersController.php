2536d26d679be25ccf78644e85819823f2dd6a0a
badaisamoedra <badai.samoedra@yahoo.co.id>
2018-10-14 16:37:34 +0700

    Update UsersController.php
    trial from github web

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -115,6 +115,7 @@ class UsersController extends Controller
     public function register(Request $r, Hash $h){
         // default password
         // return response()->json(Api::response(true,'ok',$r->input()),200);
+        dd($this->validate($r, ['name' => 'required']));
         dd($r->json());
         $pass = $h->make('kop2018');
 
